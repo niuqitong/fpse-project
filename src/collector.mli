9031007a33598769c32e9bfa6848a2fc05bfcafc
Jiahui Huang <52479620+Audrey-H@users.noreply.github.com>
2023-12-19 18:35:00 -0500

    Update collector.mli

--- src/collector.mli
+++ src/collector.mli
@@ -1,5 +1,3 @@
-[@@@warning "-34"]
-
 open Batteries
 
 type process_count = {
@@ -30,8 +28,6 @@ type process_stats = {
   pid: int;
   utime: int;
   stime: int;
-  (* total_cpu_time: int;
-  total_time: int; *)
   starttime: int;
   sys_uptime: float;
   vm_rss: int;
@@ -77,48 +73,39 @@ module Cpu_collector (_ : CPUReader_type) : sig
   val string_of_cpu_stats : cpu_stats -> string
 end
 
-
 module Process_count_collector(_ : ProcCountFileReader_type) : sig
   val read_process_count : unit -> process_count 
 end
 
-
 module Mem_collector(_ : MemReader_type) : sig 
   val read_memory_info : unit -> memory_info option 
 end
-
 
 module Processes_collector(_ : ProcessesFileReader_type) : sig
   val read_process_stats : int -> process_stats 
   val collect_process_stats : unit -> process_stats list 
 end
 
-
 module LoadAvg_collector(_ : LoadAvgReader_type) : sig
   val read_load_average : unit -> load_average_stats option 
 end
-
 
 module RealLoadAvgCollector : sig 
   val read_load_average : unit -> load_average_stats option 
 end
 
-
 module RealProcCountCollector : sig 
   val read_process_count : unit -> process_count 
 end
-
 
 module RealMemCollector : sig 
   val read_memory_info : unit -> memory_info option 
 end
 
-
 module RealProcessesCollector : sig 
   val read_process_stats : int -> process_stats 
   val collect_process_stats : unit -> process_stats list 
 end
-
 
 module RealCPUCollector : sig 
   val parse_cpu_stats_line : string -> cpu_stats option
